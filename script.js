5912a093c3044cf33874eeb0babe6d11af9bc023
Chiahao <chiahao.codes@gmail.com>
2022-08-08 16:57:48 -0700

    commit master

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ function move(id) {
       { duration: 0.15, y: `45%`, opacity: 0, ease: "ease-out" },
       0
     );
+    timeline.to(`.menuElement`,
+      {
+        duration: 0.05,
+        y: `45%`,
+        fontSize: `calc(1.6em + .25vmin)`,
+        color: `white`,
+        opacity: 0.55,
+      },
+      0
+    );
 }
 
 
@@ -35,10 +45,7 @@ function move(id) {
       0
     );
     timeLine.to(`.icon`, { duration: 0.05, opacity: 0, ease: `ease-out` }, 0);
-    timeline.to(`.menuElement`, { duration: 0.05, y: `45%`, fontSize: `calc(1.6em + .25vmin)`,
-    color: `white`,
-    opacity:.55
-    }, 0);
+    
     timeLine.to(`#menu${id}`, { duration: 0.015, opacity: 0, ease: `ease-out` }, 0);
     timeLine.to(`#bubble${id}`, {duration: 0.1, opacity: 1, y: `-10%`, boxShadow: `0 calc(1px + .0215vmin) calc(3px+.0122vmin) rgba(0, 0, 0, 0.12),
     0 calc(1px + .0215vmin) calc(3px+.0202vmin) rgba(0, 0, 0, 0.24);`, ease: `ease-out`
