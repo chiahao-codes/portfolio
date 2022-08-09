e81e20bc9db9eedb69e454b7b6a06e81a812a1f7
Chiahao <chiahao.codes@gmail.com>
2022-08-08 20:28:50 -0700

    commit master

--- script.js
+++ script.js
@@ -21,13 +21,30 @@ function move(id) {
 }
 
 function boxShadow() {
-    timeLine.to(`#bubble1`, {duration:0.1,
-        backgroundColor: `hsl(0, 0 %, 11 %)`,
-    y:`-10%`,
-    boxShadow: `0 calc(1px + .0215vmin) calc(3px+.0122vmin) rgba(0, 0, 0, 0.12),
-        0 calc(1px + .0215vmin) calc(3px+.0202vmin) rgba(0, 0, 0, 0.24)`
-    ,ease:`ease-out`}, 0)
-    .to(`#bubble1>.icon`, {duration:.1, opacity:1}, 0)
+    
+    timeLine
+      .to(
+        `#bubble1`,
+        {
+          duration: 0.1,
+          backgroundColor: `hsl(0, 0 %, 11 %)`,
+          y: `-10%`,
+          boxShadow: `0 calc(1px + .0215vmin) calc(3px+.0122vmin) rgba(0, 0, 0, 0.12), 0 calc(1px + .0215vmin) calc(3px+.0202vmin) rgba(0, 0, 0, 0.24)`,
+          ease: `ease-out`,
+        },
+        0
+      )
+      .to(`#bubble1>.icon`, { duration: 0.1, opacity: 1 }, 0)
+      .to(
+        "#bgBubble1",
+        { duration: 0.2, left: "10%", ease: "ease-in-out" },
+        0.1
+      )
+      .to(
+        "#bgBubble1",
+        { duration: 0.15, bottom: "-10%", ease: "ease-out" },
+        "-=0.2"
+      );
 }
 
 /**
