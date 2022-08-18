8adf7118698a5afc32e3fda3197546013b9857cd
Chiahao <chiahao.codes@gmail.com>
2022-08-18 16:17:49 -0700

    commit master

--- script.js
+++ script.js
@@ -52,55 +52,6 @@ function move(id, position) {
         "-=0.1"
   )
     .to(`#menu${id}`, { duration: 0.15, opacity: 0, ease: "ease-out" }, "-=0.1")
-    .to(`#menu${id}:hover`, {duration:0.1, cursor:"default", ease:"ease-out"}, "-=0.1")
+    .to(`#menu${id}:hover`, {duration:0.1, cursor:"default", ease:"ease-out"}, )
 }
 
-
-
-/**
- * timeLine.to(`#bubble1`, { duration: .1, y: `110%`, boxShadow: `none`, ease: `ease-out` }, 0);
-    timeLine.to( `#bubble2`, { duration: 0.1, y: `110%`, boxShadow: `none`, ease: `ease-out`, },0);
-    timeLine.to(
-      `#bubble3`,
-      {
-        duration: 0.1,
-        y: `110%`,
-        boxShadow: `none`,
-    
-        ease: `ease-out`,
-      },
-      0
-    );
-    timeLine.to(
-      `#bubble4`,
-      {
-        duration: 0.1,
-        y: `110%`,
-        boxShadow: `none`,
-    
-        ease: `ease-out`,
-      },
-      0
-    );
-    timeLine.to(`.icon`, { duration: 0.05, opacity: 0, ease: `ease-out` }, 0);
-    
-    timeLine.to(`#menu${id}`, { duration: 0.015, opacity: 0, ease: `ease-out` }, 0);
-    timeLine.to(`#bubble${id}`, {duration: 0.1, opacity: 1, y: `-10%`, boxShadow: `0 calc(1px + .0215vmin) calc(3px+.0122vmin) rgba(0, 0, 0, 0.12),
-    0 calc(1px + .0215vmin) calc(3px+.0202vmin) rgba(0, 0, 0, 0.24);`, ease: `ease-out`
-    }, `-=1`);
-    timeLine.to(`#bubble${id} > .icon`, { duration: .1, opacity: 1, ease: `ease-in` },0);
-    timeLine.to(
-      `.navBar > #bubbleWrapper > #bgBubble${id}`,
-      {
-        duration: 0.1,
-        y: `0%`,
-        opacity: 1,
-        backgroundColor: `black`,
-        borderRadius: `50%`,
-        width: `calc(70px + .025vmin)`,
-        height:`calc(70px + .025vmin)`,
-        ease: "ease-out",
-      },
-      0
-    );
- */
