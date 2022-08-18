a59900536d5ac0f6d8cfedcb1f3c58ab0d42e6e5
Chiahao <chiahao.codes@gmail.com>
2022-08-18 16:13:40 -0700

    commit master

--- script.js
+++ script.js
@@ -39,13 +39,20 @@ function move(id, position) {
         {
           duration: 0.15,
           y: "0%",
-          backgroundColor:"hsl(0, 0%, 12%)",
+          backgroundColor: "hsl(0, 0%, 12%)",
           opacity: 1,
           boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
           ease: "ease-out",
         },
         "-=0.1"
-      );
+      )
+      .to(
+        `#bubble${id}> span`,
+        { duration: 0.15, y: "0%", opacity: 1, ease: "ease-out" },
+        "-=0.1"
+  )
+    .to(`#menu${id}`, { duration: 0.15, opacity: 0, ease: "ease-out" }, "-=0.1")
+    .to(`#menu${id}:hover`, {duration:0.1, cursor:"default", ease:"ease-out"}, "-=0.1")
 }
 
 
