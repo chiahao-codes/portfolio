3b136607933aaa3a321c7008a248428c599f2195
Chiahao <chiahao.codes@gmail.com>
2022-09-15 13:33:33 -0700

    commit master

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener("load", () => {
   getViewPort();
   localStorage.clear();
   console.log(`viewport: ${viewport}`);
-  currBgBubblePercent = "10.98%";
+  currBgBubblePercent = "10.99%";
   moveBgBubbleAndTurnOffElem(1, currBgBubblePercent, bgBubble);
   giveMenuIdClickEvent(bgBubble);
   resizeBgbLeft(bgBubble, viewport, currBgBubblePercent);
@@ -27,7 +27,7 @@ let menuElements = document.getElementsByClassName("menuElement");
 let viewport, currBgBubblePercent, id;
 
 let percentMap = {
-  0: "10.98%",
+  0: "10.99%",
   1: "36%",
   2: "61%",
   3: "86.2%",
