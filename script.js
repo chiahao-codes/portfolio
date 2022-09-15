6c24bd24d8401a1204af55e45b6d2303aade4574
Chiahao <chiahao.codes@gmail.com>
2022-09-15 13:23:30 -0700

    commit master

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ let menuElements = document.getElementsByClassName("menuElement");
 let viewport, currBgBubblePercent, id;
 
 let percentMap = {
-  0: "10.2%",
+  0: "11.2%",
   1: "36%",
   2: "61%",
   3: "86.2%",
