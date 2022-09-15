f126a82ec53d5a47b5c2aa44190e2c0d2993d2b6
Chiahao <chiahao.codes@gmail.com>
2022-09-15 15:59:31 -0700

    commit master

--- script.js
+++ script.js
@@ -158,19 +158,27 @@ function gsapAnimationResizeBgbLeft(bgB, view, position, timeLine) {
   console.log(`viewport: ${view}`);
   if (view >= 1650) {
     if (position !== "86.2%") {
-      let incrementPosition = parseInt(position) + 0.3;
-      incrementPosition = incrementPosition.toString();
-      position = `${incrementPosition}%`;
+      timeLine.to(
+        bgB,
+        {
+          duration: 0.219,
+          left: `calc(${position} - .6vw)`,
+          ease: "ease-in-out",
+        },
+        0
+      );
+      
+    } else {
+      timeLine.to(
+        bgB,
+        {
+          duration: 0.219,
+          left: `calc(${position} - .63vw)`,
+          ease: "ease-in-out",
+        },
+        0
+      );
     }
-    timeLine.to(
-      bgB,
-      {
-        duration: 0.219,
-        left: `calc(${position} - .63vw)`,
-        ease: "ease-in-out",
-      },
-      0
-    );
   }
   if (view >= 1600) {
     timeLine.to(
