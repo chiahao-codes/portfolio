d5a30e1977c88e655e4e7cee2e27b8664c0e08d4
Chiahao <chiahao.codes@gmail.com>
2022-09-16 15:06:46 -0700

    commit master

--- script.js
+++ script.js
@@ -222,7 +222,7 @@ function gsapAnimationResizeBgbLeft(bgB, view, position, timeLine) {
   if (view >= 1500 && view <= 1549.99) {
     if (position === "86.2%") {
        positionNumber = parseInt(position);
-       positionNumber = (positionNumber - .05).toString();
+       positionNumber = (positionNumber - .65).toString();
        position = `${positionNumber}%`;
     }
     timeLine.to(
@@ -452,7 +452,7 @@ positionNumber = parseInt(position);
   if (view >= 1500 && view <= 1549.99) {
     if (position === "86.2%") {
       positionNumber = parseInt(position);
-      positionNumber = (positionNumber - .05).toString();
+      positionNumber = (positionNumber - .06).toString();
       position = `${positionNumber}%`;
     }
     timeLine.to(
