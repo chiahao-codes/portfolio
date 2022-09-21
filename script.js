da15138c9237ed7072076c4bd5f41867b8430744
Chiahao <chiahao.codes@gmail.com>
2022-09-21 00:24:55 -0700

    commit master

--- script.js
+++ script.js
@@ -492,7 +492,7 @@ function gsapAnimationResizeBgbLeft(bgB, view, position, timeLine) {
 
 function onResize() {
   console.log("on resize running...")
-  console.log(bgBubble.style)
+  console.log(bgBubble.style.left = "0%");
 }
 
 function resizeBgbLeft(bgB = bgBubble, view = viewport, position = currBgBubblePercent) {
