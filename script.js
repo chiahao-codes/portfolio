2871c4c92193c468e2c0d04d1c0ce18e5f261f85
Chiahao <chiahao.codes@gmail.com>
2022-09-21 00:09:03 -0700

    commit master

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ function move(id, position, bgBubble) {
   timeLine
     .to(
       bgBubble,
-      { duration: 0.2, bottom: "calc(-40px + .025vmin)", ease: "ease-out" },
+      { duration: 0.1, bottom: "calc(-40px + .025vmin)", ease: "ease-out" },
       0
     )
     .to(
