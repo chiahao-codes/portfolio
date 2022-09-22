401b1f1d5e04e98d32f92f3511e6b41533a78809
Chiahao <chiahao.codes@gmail.com>
2022-09-22 14:48:12 -0700

    commit master

--- script.js
+++ script.js
@@ -95,22 +95,22 @@ function move(id, position, bgBubble) {
     )
     .to(
       "#bubble1",
-      { duration: 0.1, y: "175%", boxShadow: "none", ease: "ease-out" },
+      { duration: 0.1, y: "15%", boxShadow: "none", ease: "ease-out" },
       0
     )
     .to(
       "#bubble2",
-      { duration: 0.1, y: "175%", boxShadow: "none", ease: "ease-out" },
+      { duration: 0.1, y: "15%", boxShadow: "none", ease: "ease-out" },
       0
     )
     .to(
       "#bubble3",
-      { duration: 0.1, y: "175%", boxShadow: "none", ease: "ease-out" },
+      { duration: 0.1, y: "15%", boxShadow: "none", ease: "ease-out" },
       0
     )
     .to(
       "#bubble4",
-      { duration: 0.1, y: "175%", boxShadow: "none", ease: "ease-out" },
+      { duration: 0.1, y: "15%", boxShadow: "none", ease: "ease-out" },
       0
     )
     .to(
