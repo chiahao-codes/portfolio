4a6755ba561a3b42b894c08a6d5f29f304d28c2e
Chiahao <chiahao.codes@gmail.com>
2022-10-16 02:27:05 -0700

    commit master

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-
+window.addEventListener("load", () => {
+  setHomePage();
+  darkMode();
+})
 let body = document.getElementsByTagName("body")[0];
 let navBar = document.getElementById("navBar");
 let homeContainer = document.getElementById("homeContainer");
 let portfolioContainer = document.querySelector("body > section");
 let switchBulbWrap = document.getElementById("switch_bulb_wrapper");
-let darkBulbIcon = document.querySelector("div > .darkbulb");
-let lightBulbIcon = document.querySelector("div > .lightbulb");
-let modeSwitchWrapper = document.getElementById("switch_bulb_wrapper");
+let switchCircle = document.querySelector(".switch_circle");
+console.log(switchCircle);
+let modeSwitchContainer = document.getElementById("mode_switch_container");
+let menuIcon = document.querySelectorAll("#menuIconWrapper > .menuIcon");
 let tl = gsap.timeline();
 
 function setHomePage() {
@@ -14,12 +18,22 @@ function setHomePage() {
   homeContainer.style.display = "flex";
   homeContainer.style.justifyContent = "center";
   homeContainer.style.alignItems = "center";
-  
   homeContainer.style.top = "30vh";
   portfolioContainer.style.display = "none";
-  //body.style.backgroundColor = "white";
+}
 
-  //#1a1a1a#ebeded navBar.style.backgroundColor = "";
+//#ebeded 
+
+function darkMode() {
+  homeContainer.style.color = "white";
+  body.style.backgroundColor = "#161617";
+  navBar.style.backgroundColor = "#1a1a1a";
+  modeSwitchContainer.style.backgroundColor = "black";
+  switchCircle.style.marginRight = "2%";
+  switchCircle.style.filter = "invert(1)";
+  menuIcon.forEach((m) => {
+    m.style.backgroundColor = "white";
+  });
 }
 
 
