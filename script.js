bc42612afcc91a18966fa7075d00dfa6c4a3eb6b
Chiahao <chiahao.codes@gmail.com>
2022-10-19 02:55:57 -0700

    commit master

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function darkMode() {
   modeSwitchContainer.style.transitionTimingFunction = "ease-in";
 
   
-  switchCircle.style.transform = "translateX(25px)";
+  switchCircle.style.transform = "translateX(28px)";
   switchCircle.style.filter = "invert(1)";
   switchCircle.style.transitionProperty = "transform,filter";
   switchCircle.style.transitionDuration = "100ms";
@@ -85,7 +85,7 @@ function lightMode() {
     modeSwitchContainer.style.transitionTimingFunction = "ease-in";
    
 
-  switchCircle.style.transform = "translateX(-25px)"
+  switchCircle.style.transform = "translateX(-28px)"
  
     switchCircle.style.filter = "invert(0)";
     switchCircle.style.transitionProperty = "transform, filter";
