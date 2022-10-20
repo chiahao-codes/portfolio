aff72b8ea8b2900c284cd08cd1a14c1b8dc5f9d3
Chiahao <chiahao.codes@gmail.com>
2022-10-20 16:54:08 -0700

    commit master

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let switchCircle = document.querySelector("#navBar > #mode_switch_social_wrapper
 let modeSwitchContainer = document.getElementsByClassName("mode_switch_container")[0];
 let socialIcons = document.querySelectorAll(".social_icon_parent > a > .social_icon");
 let navLinksWrapper = document.querySelectorAll("#navBar > #navigation_links_wrapper")[0];
+let navLinks = navLinksWrapper.children;
+
 let menuIcon = document.querySelectorAll("#menuIconWrapper > .menuIcon");
 let mode;
 /***/
@@ -50,11 +52,19 @@ function darkMode() {
   switchCircle.style.transitionTimingFunction = "ease-in";
 
   navLinksWrapper.style.color = "white";
+  navLinksWrapper.style.transitionProperty = "color";
+  navLinksWrapper.style.transitionDuration = "100ms";
+  navLinksWrapper.style.transitionTimingFunction = "ease-in";
 
    homeContainer.style.color = "white";
    homeContainer.style.transitionProperty = "color";
    homeContainer.style.transitionDuration = "100ms";
-   homeContainer.style.transitionTimingFunction = "ease-in";
+  homeContainer.style.transitionTimingFunction = "ease-in";
+  
+  for (let n of navLinks) {
+    n.style.filter = "brightness(110%), contrast(110%)";
+    n.style.fontWeight = "600";
+  }
   
   for (let s of socialIcons) {
     s.style.filter = "invert(1)";
@@ -98,11 +108,19 @@ function lightMode() {
     switchCircle.style.transitionTimingFunction = "ease-in";
 
   navLinksWrapper.style.color = "black";
+  navLinksWrapper.style.transitionProperty = "color";
+  navLinksWrapper.style.transitionDuration = "100ms";
+  navLinksWrapper.style.transitionTimingFunction = "ease-in";
   
     homeContainer.style.color = "black";
     homeContainer.style.transitionProperty = "color";
     homeContainer.style.transitionDuration = "100ms";
   homeContainer.style.transitionTimingFunction = "ease-in";
+
+   for (let n of navLinks) {
+     n.style.filter = "brightness(110%), contrast(110%)";
+     n.style.fontWeight = "600";
+   }
   
    for (let s of socialIcons) {
      s.style.filter = "invert(0)";
