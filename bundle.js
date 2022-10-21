9a2d490bc194beb6fa7494883adf25e85ba3897f
Chiahao <chiahao.codes@gmail.com>
2022-10-20 17:39:05 -0700

    commit browser

--- bundle.js
+++ bundle.js
@@ -41,7 +41,7 @@ function darkMode() {
   navBar.style.transitionDuration = "100ms";
   navBar.style.transitionTimingFunction = "ease-in";
   navBar.style.backgroundColor = "#1a1a1a";
-  
+
   modeSwitchContainer.style.backgroundColor = "black";
   modeSwitchContainer.style.transitionProperty = "background-color";
   modeSwitchContainer.style.transitionDuration = "100ms";
@@ -58,86 +58,85 @@ function darkMode() {
   navLinksWrapper.style.transitionDuration = "100ms";
   navLinksWrapper.style.transitionTimingFunction = "ease-in";
 
-   homeContainer.style.color = "white";
-   homeContainer.style.transitionProperty = "color";
-   homeContainer.style.transitionDuration = "100ms";
+  homeContainer.style.color = "white";
+  homeContainer.style.transitionProperty = "color";
+  homeContainer.style.transitionDuration = "100ms";
   homeContainer.style.transitionTimingFunction = "ease-in";
-  
+
   for (let n of navLinks) {
     n.style.filter = "brightness(110%), contrast(110%)";
     n.style.fontWeight = "600";
   }
-  
-  for (let s of socialIcons) {
+  /**
+   * for (let s of socialIcons) {
     s.style.filter = "invert(1)";
     s.style.transitionProperty = "filter";
     s.style.transitionDuration = "100ms";
     s.style.transitionTimingFunction = "ease-in";
   }
-  
+   * 
+   */
+
   for (let m of menuIcon) {
-        m.style.backgroundColor = "white";
-        m.style.transitionProperty = "background-color";
-        m.style.transitionDuration = "100ms";
-        m.style.transitionTimingFunction = "ease-in";
+    m.style.backgroundColor = "white";
+    m.style.transitionProperty = "background-color";
+    m.style.transitionDuration = "100ms";
+    m.style.transitionTimingFunction = "ease-in";
   }
- 
 }
 
 function lightMode() {
- 
   body.style.backgroundColor = "white";
   body.style.transitionProperty = "background-color";
   body.style.transitionDuration = "100ms";
   body.style.transitionTimingFunction = "ease-in";
-  
-  
-    navBar.style.transitionProperty = "background-color";
-    navBar.style.transitionDuration = "100ms";
-    navBar.style.transitionTimingFunction = "ease-in";
-    navBar.style.backgroundColor = "#ebeded";
 
-    modeSwitchContainer.style.backgroundColor = "#dcdcde";
-    modeSwitchContainer.style.transitionProperty = "background-color";
-    modeSwitchContainer.style.transitionDuration = "100ms";
-    modeSwitchContainer.style.transitionTimingFunction = "ease-in";
-   
+  navBar.style.transitionProperty = "background-color";
+  navBar.style.transitionDuration = "100ms";
+  navBar.style.transitionTimingFunction = "ease-in";
+  navBar.style.backgroundColor = "#ebeded";
 
-    switchCircle.style.transform = "translateX(-28.5px)"
-    switchCircle.style.filter = "invert(0)";
-    switchCircle.style.transitionProperty = "transform, filter";
-    switchCircle.style.transitionDuration = "100ms";
-    switchCircle.style.transitionTimingFunction = "ease-in";
+  modeSwitchContainer.style.backgroundColor = "#dcdcde";
+  modeSwitchContainer.style.transitionProperty = "background-color";
+  modeSwitchContainer.style.transitionDuration = "100ms";
+  modeSwitchContainer.style.transitionTimingFunction = "ease-in";
+
+  switchCircle.style.transform = "translateX(-28.5px)";
+  switchCircle.style.filter = "invert(0)";
+  switchCircle.style.transitionProperty = "transform, filter";
+  switchCircle.style.transitionDuration = "100ms";
+  switchCircle.style.transitionTimingFunction = "ease-in";
 
   navLinksWrapper.style.color = "black";
   navLinksWrapper.style.transitionProperty = "color";
   navLinksWrapper.style.transitionDuration = "100ms";
   navLinksWrapper.style.transitionTimingFunction = "ease-in";
-  
-    homeContainer.style.color = "black";
-    homeContainer.style.transitionProperty = "color";
-    homeContainer.style.transitionDuration = "100ms";
+
+  homeContainer.style.color = "black";
+  homeContainer.style.transitionProperty = "color";
+  homeContainer.style.transitionDuration = "100ms";
   homeContainer.style.transitionTimingFunction = "ease-in";
 
-   for (let n of navLinks) {
-     n.style.filter = "brightness(110%), contrast(110%)";
-     n.style.fontWeight = "600";
-   }
-  
-   for (let s of socialIcons) {
+  for (let n of navLinks) {
+    n.style.filter = "brightness(110%), contrast(110%)";
+    n.style.fontWeight = "600";
+  }
+
+  /**
+   * for (let s of socialIcons) {
      s.style.filter = "invert(0)";
     s.style.transitionProperty = "filter";
     s.style.transitionDuration = "100ms";
     s.style.transitionTimingFunction = "ease-in";
    }
+   */
 
-   for (let m of menuIcon) {
-     m.style.backgroundColor = "black";
-     m.style.transitionProperty = "background-color";
-     m.style.transitionDuration = "100ms";
-     m.style.transitionTimingFunction = "ease-in";
-   }
-    
+  for (let m of menuIcon) {
+    m.style.backgroundColor = "black";
+    m.style.transitionProperty = "background-color";
+    m.style.transitionDuration = "100ms";
+    m.style.transitionTimingFunction = "ease-in";
+  }
 }
 
 
