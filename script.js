98862157a1d5c2a1a8cdd3daa4c3d02e2424bb46
Chiahao <chiahao.codes@gmail.com>
2022-11-10 19:50:03 -0800

    commit master

--- script.js
+++ script.js
@@ -358,8 +358,8 @@ mobileSwitchContainer.addEventListener("click", () => {
  * 
  */
 
-resumeButtonContainer.addEventListener("click", () => {
-  resumeButtonContainer.style.transform = "scale(.9)";
+resumeButton.addEventListener("click", () => {
+  resumeButton.style.transform = "scale(.9)";
   resumeButtonContainer.style.boxShadow = "0px 1px 11px rgba(0, 0, 0, 0.35)";
   },
 );
